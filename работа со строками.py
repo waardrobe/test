ae9dd1ac819624e27884a2c07efb55209b8ba9f9
waardrobe <Kirill814814@yandex.ru>
2022-07-22 19:07:16 +0400

    asd

---
+++ работа со строками.py
@@ -0,0 +1,24 @@
+print("Привет\nПривет\tПривет")
+
+print('"текст в кавычках"')
+print("текст \"в\" кавычках")
+
+text = "Привет"
+text1 = "Мир"
+print(text + ' ' + text1)
+print(text*5)
+print(text[0:3])
+print(text.upper())
+print(text.lower())
+print(text.capitalize())
+print(text.split(" "))
+
+
+spisok = ["a", "b", "c"]
+print(",".join(spisok))
+
+text = "     lajfk jfklsdjfs aosdks'l                   "
+print(text.strip())
+
+text = "ololololololololololo"
+print(text.replace("l", "o"))
